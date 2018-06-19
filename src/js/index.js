958ebd558e5a6526e3b116e1db12b6dd8c19ebf2
Daniel <dyrda@in.tum.de>
2018-06-19 11:40:30 +0200

    added getDeviceIdentifier(msg) method and added export of constants

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
- 
+const {topicSeparator, deviceIdPrefix} = require('./constants.js');
+
+let getDeviceIdentifier = function(msg){
+    if(msg.includes(deviceIdPrefix)){
+        return '';
+    }
+    let fields = msg.split(deviceIdPrefix);
+    return fields[fields.length - 1 ];
+}
+
+module.exports = {
+    'topicSeparator': topicSeparator,
+}
