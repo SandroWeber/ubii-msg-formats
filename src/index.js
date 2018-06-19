1fdf8484df19da3f5031b7352a2290aa14ddb0e1
Daniel <dyrda@in.tum.de>
2018-06-19 21:52:03 +0200

    added extractTopic and extractValue methods

--- src/index.js
+++ src/index.js
@@ -16,6 +16,31 @@ let extractMessageType = function(msg){
     return fields[0];
 }
 
+let extractTopic = function(msg){
+    if(!msg.includes(msgTypeSeparator)){
+        return '';
+    }
+    let fields = msg.toString().split(msgTypeSeparator);
+
+    if(!fields[1].includes(valueTypeSeparator)){
+        return '';
+    }
+    fields = fields[1].toString().split(valueTypeSeparator);
+    return fields[0];
+}
+let extractValue = function(msg){
+    if(!msg.includes(valueTypeSeparator)){
+        return '';
+    }
+    let fields = msg.toString().split(valueTypeSeparator);
+
+    if(!fields[1].includes(deviceIdPrefix)){
+        return '';
+    }
+    fields = fields[1].toString().split(deviceIdPrefix);
+    return fields[0];
+}
+
 let removeMessageType = function(msg){
     if(!msg.includes(msgTypeSeparator)){
         return '';
@@ -30,6 +55,8 @@ module.exports = {
     'valueTypeSeparator': valueTypeSeparator,
     'deviceIdPrefix': deviceIdPrefix,
     'extractDeviceIdentifier': extractDeviceIdentifier,
+    'extractTopic': extractTopic,
     'extractMessageType': extractMessageType,
+    'extractValue': extractValue,
     'removeMessageType': removeMessageType,
 }
