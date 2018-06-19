a1eb7fd2d55709c6ca5a75c37512ec48fcd1619a
Daniel <dyrda@in.tum.de>
2018-06-19 17:53:56 +0200

    added getMsgType and removeMsgType methods

--- src/index.js
+++ src/index.js
@@ -8,10 +8,28 @@ let getDeviceIdentifier = function(msg){
     return fields[fields.length - 1 ];
 }
 
+let getMessageType = function(msg){
+    if(!msg.includes(msgTypeSeparator)){
+        return '';
+    }
+    let fields = msg.split(msgTypeSeparator);
+    return fields[0];
+}
+
+let removeMessageType = function(msg){
+    if(!msg.includes(msgTypeSeparator)){
+        return '';
+    }
+   
+    return msg.split(msgTypeSeparator).pop();
+}
+
 module.exports = {
     'msgTypeSeparator': msgTypeSeparator,
     'topicSeparator': topicSeparator,
     'valueTypeSeparator': valueTypeSeparator,
     'deviceIdPrefix': deviceIdPrefix,
     'getDeviceIdentifier': getDeviceIdentifier,
+    'getMessageType': getMessageType,
+    'removeMessageType': removeMessageType,
 }
