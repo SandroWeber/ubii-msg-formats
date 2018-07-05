7fd7fdec930730229506f8300fda6010a0f83555
Daniel <dyrda@in.tum.de>
2018-07-05 13:25:39 +0200

    removed legacy test from main test file

--- test/test.js
+++ test/test.js
@@ -1,5 +1,4 @@
 import test from 'ava';
-import {extractDeviceIdentifier, extractTopic, extractValue, topicSeparator, valueSeparator, deviceIdPrefix} from '../src/index';
 import {TopicDataBlockMessage} from '../src/index';
 
 (function(){
@@ -26,54 +25,5 @@ import {TopicDataBlockMessage} from '../src/index';
         
         
     });
-
-    // Legacy implementation tests
-	/*test('extractDeviceIdentifier', t => {
-        let testString = `player1${topicSeparator}left${topicSeparator}arm${valueSeparator}0${deviceIdPrefix}kinect1`;
-        t.is(extractDeviceIdentifier(testString), 'kinect1');
-    
-        testString = `player1${topicSeparator}left${topicSeparator}arm${valueSeparator}0${deviceIdPrefix}56463653:kinect1`;
-        t.is(extractDeviceIdentifier(testString), '56463653:kinect1');
-
-        testString = `player1${topicSeparator}left${topicSeparator}arm${valueSeparator}0${deviceIdPrefix}whitespace${deviceIdPrefix}56463653:kinect1`;
-        t.is(extractDeviceIdentifier(testString), '56463653:kinect1');
-
-        testString = `player1${topicSeparator}left${topicSeparator}arm${valueSeparator}0`;
-        t.is(extractDeviceIdentifier(testString), '');
-    });
-
-    test('extractValue', t => {
-        let testString = `player1${topicSeparator}left${topicSeparator}arm${valueSeparator}0${deviceIdPrefix}kinect1`;
-        t.is(extractValue(testString), '0');
-    
-        testString = `player1${topicSeparator}left${topicSeparator}arm${valueSeparator}1${deviceIdPrefix}56463653:kinect1`;
-        t.is(extractValue(testString), '1');
-
-        testString = `player1${topicSeparator}left${topicSeparator}arm${valueSeparator}2${deviceIdPrefix}whitespace${deviceIdPrefix}56463653:kinect1`;
-        t.is(extractValue(testString), '2');
-
-        testString = `player1${topicSeparator}left${topicSeparator}arm${valueSeparator}0`;
-        t.is(extractValue(testString), '');
-
-        testString = `player1${topicSeparator}left${topicSeparator}arm${deviceIdPrefix}56463653:kinect1`;
-        t.is(extractValue(testString), '');
-    });
-
-    test('extractTopic', t => {
-        let testString = `player1${topicSeparator}left${topicSeparator}arm${valueSeparator}0${deviceIdPrefix}kinect1`;
-        t.is(extractTopic(testString), `player1${topicSeparator}left${topicSeparator}arm`);
-    
-        testString = `player1${topicSeparator}left${topicSeparator}arm${valueSeparator}1${deviceIdPrefix}56463653_kinect1`;
-        t.is(extractTopic(testString), `player1${topicSeparator}left${topicSeparator}arm`);
-
-        testString = `player1${topicSeparator}left${topicSeparator}arm${valueSeparator}2${deviceIdPrefix}whitespace${deviceIdPrefix}56463653_kinect1`;
-        t.is(extractTopic(testString), `player1${topicSeparator}left${topicSeparator}arm`);
-
-        testString = `player1${topicSeparator}left${topicSeparator}arm${valueSeparator}0`;
-        t.is(extractTopic(testString), `player1${topicSeparator}left${topicSeparator}arm`);
-
-        testString = `player1${topicSeparator}left${topicSeparator}arm${deviceIdPrefix}56463653_kinect1`;
-        t.is(extractTopic(testString), '');
-    });*/
     
 })();
