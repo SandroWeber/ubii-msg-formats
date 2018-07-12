6d43dadbbf80f3b708b03515d1fa17ae2a9489b6
Daniel <dyrda@in.tum.de>
2018-07-12 18:56:26 +0200

    export new carrierMessage in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const TopicDataMessageTranslator = require('./topicDataMessage/topicDataMessageTranslator.js');
+const carrierMessageTranslator = require('./carrierMessage/carrierMessageTranslator.js');
 
 module.exports =  {
     TopicDataMessageTranslator: TopicDataMessageTranslator,
+    carrierMessageTranslator: carrierMessageTranslator,
 };
