39d9b4f4488766f98ec072ceae6f75012c1a482e
daniel <dyrda@in.tum.de>
2018-08-09 14:29:38 +0200

    implemented subscribtion message

---
+++ src/js/messageTranslator/subscribtionMessageTranslator.js
@@ -0,0 +1,40 @@
+const MessageTranslator = require('./messageTranslator');
+
+/**
+ * Message translator for topic data input and output messages.
+ */
+class SubscribtionMessageTranslator extends MessageTranslator {
+
+    constructor(loadProtoFileSynchronously = true) {
+        super(__dirname+'/../../proto/topicDataMessage.proto', 'topicDataMessage', loadProtoFileSynchronously);
+    }
+
+    createPayload(deviceIdentifier, repeatedSubscribeTopicData = [], repeatedUnsubscribeTopicData = []) {
+        let payload = {
+            messageType: 'subscribtion',
+            deviceIdentifier: ''+deviceIdentifier,
+            subscribeTopicData: repeatedSubscribeTopicData,
+            unsubscribeTopicData: repeatedUnsubscribeTopicData,
+        }
+
+        return payload;
+    }
+
+    createSubscribeTopicDataPayload(topic) {
+        let payload = {          
+            topic: JSON.stringify(topic)
+        }
+
+        return payload;
+    }
+
+    createUnsubscribeTopicDataPayload(topic) {
+        let payload = {          
+            topic: JSON.stringify(topic)
+        }
+
+        return payload;
+    }
+}
+
+module.exports = SubscribtionMessageTranslator;
