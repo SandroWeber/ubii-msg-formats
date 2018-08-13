dba5b37bb9558ea7b10222d67df2c02fbd7fa8a2
Daniel <dyrda@in.tum.de>
2018-08-13 12:33:45 +0200

    Exporting SubscribtionMessageTranslator

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 const TopicDataMessageTranslator = require('./messageTranslator/topicDataMessageTranslator');
+const SubscribtionMessageTranslator = require('./messageTranslator/subscribtionMessageTranslator');
 const RegistrationMessageTranslator = require('./messageTranslator/registrationMessageTranslator');
 const UbiiMessageTranslator = require('./messageTranslator/ubiiMessageTranslator');
 
 module.exports =  {
     TopicDataMessageTranslator: TopicDataMessageTranslator,
+    SubscribtionMessageTranslator: SubscribtionMessageTranslator,
     RegistrationMessageTranslator: RegistrationMessageTranslator,
     UbiiMessageTranslator: UbiiMessageTranslator,
 };
