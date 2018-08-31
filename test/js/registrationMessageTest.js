d60b3f5f751970cea4e0338ff24f35028406827f
Daniel <dyrda@in.tum.de>
2018-08-31 14:13:34 +0200

    Added registrationMessagetest file and implemneted basic test

---
+++ test/js/registrationMessageTest.js
@@ -0,0 +1,27 @@
+import test from 'ava';
+import {
+    UbiiMessageTranslator,
+} from '../../src/js/index';
+
+(function () {
+    test('registrationmessage - Basics', t => {
+        let translator = new UbiiMessageTranslator();
+
+        let message = translator.createMessageFromPayload(translator.createPayload({
+            registrationMessage: {
+                messageType: 'registration',
+                deviceIdentifier: 'uniqueId',
+                deviceType: 0
+            }
+        }));
+
+        let result = translator.createBufferFromMessage(message);
+        result = translator.createMessageFromBuffer(result);
+
+        t.is(result.topicDataMessage, null);
+        t.not(result.registrationMessage, null);
+
+        t.is(result.registrationMessage.deviceType, 0);
+    });
+
+})();
