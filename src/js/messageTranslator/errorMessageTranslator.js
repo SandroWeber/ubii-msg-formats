a33908c509c50c13a2ba6f5be1b4167c4dd043e3
Daniel <dyrda@in.tum.de>
2018-09-04 12:39:47 +0200

    Added js dummy implementation of errorMessages

---
+++ src/js/messageTranslator/errorMessageTranslator.js
@@ -0,0 +1,21 @@
+const MessageTranslator = require('./messageTranslator');
+
+/**
+ * Message translator for error messages.
+ */
+class ErrorMessageTranslator extends MessageTranslator {
+
+    constructor(loadProtoFileSynchronously = true) {
+        super(__dirname+'/../../proto/errorMessage.proto', 'errorMessage', loadProtoFileSynchronously);
+    }
+
+    createPayload(data) {
+        let payload = {
+            ...data,
+            messageType: 'error',
+        }
+        return payload;
+    }
+}
+
+module.exports = ErrorMessageTranslator;
