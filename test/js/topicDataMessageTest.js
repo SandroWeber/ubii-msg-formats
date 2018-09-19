06a5452815548645e1845de4ebcfaeca5a0e3332
daniel <dyrda@in.tum.de>
2018-09-19 16:44:09 +0200

    Refactoring of the topicDataMessage test case

--- test/js/topicDataMessageTest.js
+++ test/js/topicDataMessageTest.js
@@ -1,46 +1,24 @@
 import test from 'ava';
 import {
-    TopicDataMessageTranslator,
     UbiiMessageTranslator,
 } from '../../src/js/index';
 
 (function () {
 
-    let createRepeatedPublishTopicDataSnapshotOne = () => {
-        let translator = new TopicDataMessageTranslator();
-
-        let repeatedPublishTopicData = [];
-        repeatedPublishTopicData.push({
-            topic: 'awesomeTopic',
-            number: 30
-        });
-        repeatedPublishTopicData.push({
-            topic: 'awesomeTopic2',
-            vector3: {
-                x: 2,
-                y: 2,
-                z: 2
-            }
-        });
-
-        return repeatedPublishTopicData;
-    }
-
-    let createTopicDatMessageSnapshotOne = () => {
-        let translator = new TopicDataMessageTranslator();
-
-        return translator.createMessageFromPayload(
-            translator.createPayload({
+    let createTopicDataUbiiMessageSnapshotOne = () => {
+        return  {
+            messageType: 'ubii',
+            topicDataMessage: {
                 deviceIdentifier: 'superDevice', 
                 publishTopicData: [
                     {
                         topic: 'awesomeTopic',
-                       // data: 'number',
+                        data: 'number',
                         number: 30
                     },
                     {
                         topic: 'awesomeTopic2',
-                       // data: 'vector3',
+                        data: 'vector3',
                         vector3: {
                             x: 2,
                             y: 2,
@@ -48,14 +26,45 @@ import {
                         }
                     }
                 ]
-            })
-        );
+            }
+        };
+    };
+
+    let createTopicDataUbiiMessageOne = () => {
+        let translator = new UbiiMessageTranslator();
+
+        return translator.createMessageFromPayload(
+            translator.createPayload({
+            topicDataMessage: {
+                deviceIdentifier: 'superDevice', 
+                publishTopicData: [
+                    {
+                        topic: 'awesomeTopic',
+                        number: 30
+                    },
+                    {
+                        topic: 'awesomeTopic2',
+                        vector3: {
+                            x: 2,
+                            y: 2,
+                            z: 2
+                        }
+                    }
+                ]
+            }
+        }));
     }
 
     test('topicDataMessage - Basics', t => {
-        let translator = new TopicDataMessageTranslator();
-        let message = createTopicDatMessageSnapshotOne();
-        let repeatedPublishTopicData = createRepeatedPublishTopicDataSnapshotOne();
+        t.notThrows(() =>{
+            createTopicDataUbiiMessageOne();
+        });
+    });
+
+    test('topicDataMessage - PublishTopicData', t => {
+        let translator = new UbiiMessageTranslator();
+        let message = createTopicDataUbiiMessageOne();
+        let snapshot = createTopicDataUbiiMessageSnapshotOne();
 
         let result = translator.createBufferFromMessage(message);
         //console.log('basicTopicDataMessage: buffer: ' + result.toString());
@@ -63,19 +72,27 @@ import {
         result = translator.createMessageFromBuffer(result);
         //console.log('basicTopicDataMessage: after buffer message: ' + result.publishTopicData);
 
-        t.is(result.publishTopicData.length, repeatedPublishTopicData.length);
-
-        t.deepEqual(result.publishTopicData[0].topic, repeatedPublishTopicData[0].topic);
-        t.deepEqual(result.publishTopicData[0].data, 'number');
-        t.deepEqual(result.publishTopicData[0].number, repeatedPublishTopicData[0].number);
-        t.deepEqual(result.publishTopicData[1].topic, repeatedPublishTopicData[1].topic);
-        t.deepEqual(result.publishTopicData[1].data, 'vector3');
-        t.deepEqual(result.publishTopicData[1].vector3.x, repeatedPublishTopicData[1].vector3.x);
-        t.deepEqual(result.publishTopicData[1].vector3.y, repeatedPublishTopicData[1].vector3.y);
-        t.deepEqual(result.publishTopicData[1].vector3.z, repeatedPublishTopicData[1].vector3.z);
+        t.is(result.topicDataMessage.publishTopicData.length, snapshot.topicDataMessage.publishTopicData.length);
+        t.is(result.messageType, snapshot.messageType);
+        t.is(result.topicDataMessage.publishTopicData[0].topic, snapshot.topicDataMessage.publishTopicData[0].topic);
+        t.is(result.topicDataMessage.publishTopicData[0].data, 'number');
+        t.is(result.topicDataMessage.publishTopicData[0].number, snapshot.topicDataMessage.publishTopicData[0].number);
+        t.is(result.topicDataMessage.publishTopicData[1].topic, snapshot.topicDataMessage.publishTopicData[1].topic);
+        t.is(result.topicDataMessage.publishTopicData[1].data, 'vector3');
+        t.is(result.topicDataMessage.publishTopicData[1].vector3.x, snapshot.topicDataMessage.publishTopicData[1].vector3.x);
+        t.is(result.topicDataMessage.publishTopicData[1].vector3.y, snapshot.topicDataMessage.publishTopicData[1].vector3.y);
+        t.is(result.topicDataMessage.publishTopicData[1].vector3.z, snapshot.topicDataMessage.publishTopicData[1].vector3.z);
 
         result = translator.createPayloadFromMessage(result);
-        //console.log('basicTopicDataMessage: after toObject: ' + result.topic);
+        t.is(result.topicDataMessage.publishTopicData.length, snapshot.topicDataMessage.publishTopicData.length);
+        t.is(result.messageType, snapshot.messageType);
+        t.is(result.topicDataMessage.publishTopicData[0].topic, snapshot.topicDataMessage.publishTopicData[0].topic);
+        t.is(result.topicDataMessage.publishTopicData[0].number, snapshot.topicDataMessage.publishTopicData[0].number);
+        t.is(result.topicDataMessage.publishTopicData[1].topic, snapshot.topicDataMessage.publishTopicData[1].topic);
+        t.is(result.topicDataMessage.publishTopicData[1].vector3.x, snapshot.topicDataMessage.publishTopicData[1].vector3.x);
+        t.is(result.topicDataMessage.publishTopicData[1].vector3.y, snapshot.topicDataMessage.publishTopicData[1].vector3.y);
+        t.is(result.topicDataMessage.publishTopicData[1].vector3.z, snapshot.topicDataMessage.publishTopicData[1].vector3.z);
+
     
     });
 
