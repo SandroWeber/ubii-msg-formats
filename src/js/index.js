99015eafa796cbbe75e4e1b109dddf09251c66b4
daniel <dyrda@in.tum.de>
2018-09-19 16:58:30 +0200

    Update index exports

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,5 @@
-const TopicDataMessageTranslator = require('./messageTranslator/topicDataMessageTranslator');
-const SubscribtionMessageTranslator = require('./messageTranslator/subscribtionMessageTranslator');
-const RegistrationMessageTranslator = require('./messageTranslator/registrationMessageTranslator');
 const UbiiMessageTranslator = require('./messageTranslator/ubiiMessageTranslator');
-const ErrorMessageTranslator = require('./messageTranslator/errorMessageTranslator');
 
 module.exports =  {
-    TopicDataMessageTranslator: TopicDataMessageTranslator,
-    SubscribtionMessageTranslator: SubscribtionMessageTranslator,
-    RegistrationMessageTranslator: RegistrationMessageTranslator,
-    UbiiMessageTranslator: UbiiMessageTranslator,
-    ErrorMessageTranslator: ErrorMessageTranslator,
+    UbiiMessageTranslator: UbiiMessageTranslator
 };
