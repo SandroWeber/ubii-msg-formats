cf94f7a9998445ebb4f53787c4d569ce1596b6f6
Daniel <dyrda@in.tum.de>
2018-09-25 15:16:17 +0200

    Add valid buffer and JSON test case

--- test/js/publishTopicDataMessageTest.js
+++ test/js/publishTopicDataMessageTest.js
@@ -61,16 +61,15 @@ import {
         });
     });
 
-    test('publishTopicData', t => {
+    test('structure', t => {
         let translator = new UbiiMessageTranslator();
         let message = createMessageOne();
         let snapshot = createMessageSnapshotOne();
 
         
-        let result = translator.createBufferFromMessage(message);
-        //console.log('basicTopicDataMessage: buffer: ' + result.toString());
+        let buffer = translator.createBufferFromMessage(message);
 
-        result = translator.createMessageFromBuffer(result);
+        let result = translator.createMessageFromBuffer(buffer);
         //console.log('basicTopicDataMessage: after buffer message: ' + result.publishTopicData);
 
         t.is(result.topicDataMessage.publishTopicData.length, snapshot.topicDataMessage.publishTopicData.length);
@@ -93,6 +92,34 @@ import {
         t.is(result.topicDataMessage.publishTopicData[1].vector3.x, snapshot.topicDataMessage.publishTopicData[1].vector3.x);
         t.is(result.topicDataMessage.publishTopicData[1].vector3.y, snapshot.topicDataMessage.publishTopicData[1].vector3.y);
         t.is(result.topicDataMessage.publishTopicData[1].vector3.z, snapshot.topicDataMessage.publishTopicData[1].vector3.z);
+
+    
+    });
+
+    test('valid buffer and JSON', t => {
+        let translator = new UbiiMessageTranslator();
+        let message = createMessageOne();
+        let snapshot = createMessageSnapshotOne();
+
+        
+        let buffer = translator.createBufferFromMessage(message);
+        //console.log('basicTopicDataMessage: buffer: ' + result.toString());
+        buffer = JSON.stringify(buffer, null, 4);
+        buffer = JSON.parse(buffer, (k, v) => {
+            if (
+              v !== null            &&
+              typeof v === 'object' && 
+              'type' in v           &&
+              v.type === 'Buffer'   &&
+              'data' in v           &&
+              Array.isArray(v.data)) {
+              return new Buffer(v.data);
+            }
+            return v;
+          });
+
+        let result = translator.createMessageFromBuffer(buffer);
+        //console.log('basicTopicDataMessage: after buffer message: ' + result.publishTopicData);
 
     
     });
