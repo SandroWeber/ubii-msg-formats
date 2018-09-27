a94d9d4565364c611588a6e529ef7ea77786d1bf
Daniel <dyrda@in.tum.de>
2018-09-27 14:20:10 +0200

    Update registrationMessage test cases with new test case structure

--- test/js/registrationMessageTest.js
+++ test/js/registrationMessageTest.js
@@ -5,17 +5,17 @@ import {
 
 (function () {
 
-    let createMessageSnapshotSubscribeUnsubscribe = () => {
+    let getComparisonObject = () => {
         return  {
             messageType: 'ubii',
             registrationMessage: {
                 deviceIdentifier: 'superDevice', 
-                deviceType: 0
+                deviceType: 'PARTICIPANT'
             }
         };
     };
 
-    let createMessageBasic = () => {
+    let getMessageBasic = () => {
         let translator = new UbiiMessageTranslator();
 
         return translator.createMessageFromPayload(
@@ -27,24 +27,27 @@ import {
         }));
     }
 
+    // tests:
+
+    test.beforeEach(t => {
+        t.context.translator = new UbiiMessageTranslator();
+    });
+
     test('create basic', t => {
         t.notThrows(() =>{
-            createMessageBasic();
+            getMessageBasic(t.context);
         });
     });
 
+    test('structure', t => {
+        let messageOne = getMessageBasic(t.context);
+        let comparisonObject = getComparisonObject();
+        let buffer = t.context.translator.createBufferFromMessage(messageOne);
+        let messageTwo = t.context.translator.createMessageFromBuffer(buffer);
+        let object = t.context.translator.createPayloadFromMessage(buffer);
 
-   // test('structure', t => {
-      /*  let translator = new UbiiMessageTranslator();
-        let message = createMessageSubscribeUnsubscribe();
-        let snapshot = createMessageSnapshotSubscribeUnsubscribe();
+        t.snapshot(messageTwo); 
 
-        let result = translator.createBufferFromMessage(message);
-        //console.log('basicTopicDataMessage: buffer: ' + result.toString());
-
-        result = translator.createMessageFromBuffer(result);
-        //console.log('basicTopicDataMessage: after buffer message: ' + result.publishTopicData);
-
-       t.is(result.messageType, snapshot.messageType);*/
-    //});
+        t.true(JSON.stringify(messageTwo) === JSON.stringify(comparisonObject));
+    });
 })();
