92478ab0cb56dc30dd93e1026f21b1e4cfe66582
Daniel <dyrda@in.tum.de>
2018-11-06 17:18:00 +0100

    Added some tests

---
+++ test/js/clientSpecificationTest.js
@@ -0,0 +1,56 @@
+import test from 'ava';
+import {
+    ServiceRequestTranslator,
+} from '../../src/js';
+
+(function () {
+
+    // helepers:
+
+    let getComparisonObjectOne = () => {
+        return {
+            clientSpecification: {
+                displayName: 'awesomeDeviceName',
+                namespace: 'app',
+                id: 'uuid',
+                targetHost: 'localhost',
+                targetPort: 8667,
+            }
+        };
+    };
+
+    let getMessageOne = (context) => {
+        return context.translator.createMessageFromPayload({
+            clientSpecification: {
+                displayName: 'awesomeDeviceName',
+                namespace: 'app',
+                id: 'uuid',
+                targetHost: 'localhost',
+                targetPort: 8667,
+            }
+        });
+    }
+
+    // test cases:
+
+    test.beforeEach(t => {
+        t.context.translator = new ServiceRequestTranslator();
+    });
+
+    test('create basic', t => {
+        t.notThrows(() => {
+            getMessageOne(t.context);
+        });
+    });
+
+    test('structure', t => {
+        let messageOne = getMessageOne(t.context);
+        let comparisonObject = getComparisonObjectOne();
+        let buffer = t.context.translator.createBufferFromMessage(messageOne);
+        let messageTwo = t.context.translator.createMessageFromBuffer(buffer);
+
+        t.snapshot(messageTwo);
+
+        t.deepEqual(JSON.stringify(messageTwo) , JSON.stringify(comparisonObject));
+    });
+})();
