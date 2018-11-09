1acb0762667c49c9a5a4a4d0feb56048ca67f4ca
Sandro Weber <webers@in.tum.de>
2018-11-09 15:03:31 +0100

    tests for session

--- test/js/integration-tests/test-session-translator.js
+++ test/js/integration-tests/test-session-translator.js
@@ -4,26 +4,64 @@ import {
 } from '../../../src/js';
 
 
+let IOType = Object.freeze({'INPUT':0, 'OUTPUT':1});
+
 /* run tests */
 
 test.beforeEach(t => {
     t.context.translator = new SessionTranslator();
+
+    t.context.validPayload = {
+        name: 'session',
+        interactions: [
+            {
+                name: 'interaction1',
+                causes: [
+                    {
+                        name: 'cause1',
+                        callback: '(input, state) => { return true; }'
+                    }
+                ],
+                effects: [
+                    {
+                        name: 'effect1',
+                        callback: '(output, state) => { }'
+                    }
+                ]
+            }
+        ],
+        interactionTopicMappings: [
+            {
+                interactionIdentifier: 'interaction1',
+                topic: 'topic1',
+                ioType: IOType.INPUT,
+                internalName: 'input1'
+            }
+        ]
+    };
+
+    t.context.invalidPayload = {
+        name: 'session',
+        interactionTopicMappings: [],
+        invalidField: true
+    };
 });
 
-test('create message', t => {
-    let sessionMsg = {
-        Session: {
-            name: 'session',
-            interactions: [],
-            interaction_topic_mappings: []
-        }
-    };
-    /*let sessionMsg = {
-     name: 'session',
-     interactions: [],
-     interaction_topic_mappings: []
-     };*/
+test('create buffer from valid payload', t => {
+    let payload, buffer;
+    // valid payload
+    payload = t.context.validPayload;
+    buffer = t.context.translator.createBufferFromPayload(payload);
 
-    let protoMsg = t.context.translator.createMessageFromPayload(sessionMsg);
-    console.info(protoMsg);
+    t.is(buffer !== undefined, true);
+});
+
+test('encode buffer from valid payload, then decode buffer', t => {
+    let payload, encodedMessage, decodedMessage, buffer;
+    payload = t.context.validPayload;
+    encodedMessage = t.context.translator.createMessageFromPayload(payload);
+    buffer = t.context.translator.createBufferFromPayload(payload);
+    decodedMessage = t.context.translator.createMessageFromBuffer(buffer);
+
+    t.deepEqual(JSON.stringify(encodedMessage), JSON.stringify(decodedMessage));
 });
