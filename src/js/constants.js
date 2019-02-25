9f659dab91e3e122fac702acd6fa7d893a4fd195
Sandro Weber <webers@in.tum.de>
2019-02-25 14:26:47 +0100

    topics and msg types for sessions

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,6 +3,7 @@ const MSG_TYPES = {
   SUCCESS: 'ubii.general.Success',
   SERVICE_REQUEST: 'ubii.services.ServiceRequest',
   SERVICE_REPLY: 'ubii.services.ServiceReply',
+  SESSION: 'ubii.sessions.Session',
   TOPIC_DATA: 'ubii.topicData.TopicData'
 };
 
@@ -10,8 +11,12 @@ const DEFAULT_TOPICS = {
   SERVICES: {
     CLIENT_REGISTRATION: '/services/client_registration',
     DEVICE_REGISTRATION: '/services/device_registration',
+    SERVER_CONFIG: '/services/server_configuration',
+    SESSION_REGISTRATION: '/services/session/registration',
+    SESSION_START: '/services/session/start',
+    SESSION_STOP: '/services/session/stop',
+    TOPIC_LIST: '/services/topic_list',
     TOPIC_SUBSCRIPTION: '/services/topic_subscription',
-    SERVER_CONFIG: '/services/server_configuration'
   }
 };
 
