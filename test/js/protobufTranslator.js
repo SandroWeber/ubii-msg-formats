1d3b64cb421fef9363b96408f51ce2dc3882caa4
Sandro Weber <webers@in.tum.de>
2019-02-26 15:55:19 +0100

    extended test for ProtobufTranslator

--- test/js/protobufTranslator.js
+++ test/js/protobufTranslator.js
@@ -62,13 +62,37 @@ test('constructor() - undefined format', t => {
   });
 });
 
+test('createBufferFromMessage() -> createMessageFromBuffer()', t => {
+  t.context.messages.forEach((msg) => {
+    let translator = new ProtobufTranslator(msg.type);
+    let message = translator.createMessageFromPayload(msg.validPayload);
+
+    console.info('### createBufferFromMessage');
+
+    let buffer = undefined;
+    t.notThrows(() => {
+      buffer = translator.createBufferFromMessage(message);
+    });
+    let decodedMessage = undefined;
+    t.notThrows(() => {
+      decodedMessage = translator.createMessageFromBuffer(buffer);
+    });
+
+    t.deepEqual(JSON.stringify(decodedMessage), JSON.stringify(message));
+  })
+});
+
 test('createBufferFromPayload() -> createPayloadFromBuffer()', t => {
   t.context.messages.forEach((msg) => {
     let translator = new ProtobufTranslator(msg.type);
 
-    let buffer = translator.createBufferFromPayload(msg.validPayload);
-    let payload = translator.createPayloadFromBuffer(buffer);
+    console.info('### createBufferFromPayload');
+    t.notThrows(() => {
+      let buffer = translator.createBufferFromPayload(msg.validPayload);
+      let payload = translator.createPayloadFromBuffer(buffer);
+      console.info(payload.type);
 
-    t.deepEqual(payload, msg.validPayload);
+      t.deepEqual(payload, msg.validPayload);
+    });
   })
 });
