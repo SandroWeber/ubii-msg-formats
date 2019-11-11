a27eed8ddf732d1d28afddef6fa47abf07234509
Sandro Weber <webers@in.tum.de>
2019-11-11 15:18:27 +0100

    auto parsing and exporting of constants from .json

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,15 @@
-const proto = require('../../dist/js/protobuf');
-const ProtobufUtils = require('./protobufUtils');
-const ProtobufTranslator = require('./protobufTranslator');
-const {MSG_TYPES, DEFAULT_TOPICS} = require('./constants');
+const fs = require("fs");
 
+const proto = require("../../dist/js/protobuf");
+const ProtobufUtils = require("./protobufUtils");
+const ProtobufTranslator = require("./protobufTranslator");
+
+let constants = JSON.parse(fs.readFileSync("./dist/constants.json"));
 
 module.exports = {
   proto: proto,
   ProtobufUtils: ProtobufUtils,
   ProtobufTranslator: ProtobufTranslator,
-  MSG_TYPES: MSG_TYPES,
-  DEFAULT_TOPICS: DEFAULT_TOPICS
+  DEFAULT_TOPICS: constants.DEFAULT_TOPICS,
+  MSG_TYPES: constants.MSG_TYPES
 };
