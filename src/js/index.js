db1b6bcf604be06240eaa7a13f18eab584394091
Sandro Weber <webers@in.tum.de>
2019-11-11 15:33:47 +0100

    fixed relative path issue

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,13 @@
 const fs = require("fs");
+const path = require("path");
 
 const proto = require("../../dist/js/protobuf");
 const ProtobufUtils = require("./protobufUtils");
 const ProtobufTranslator = require("./protobufTranslator");
 
-let constants = JSON.parse(fs.readFileSync("./dist/constants.json"));
+let constants = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "../../dist/constants.json"))
+);
 
 module.exports = {
   proto: proto,
