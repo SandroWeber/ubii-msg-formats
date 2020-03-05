00f6fce5f0b679258d82f3e1ffd8268a6c4ee886
Sandro Weber <webers@in.tum.de>
2020-03-05 09:55:28 +0100

    WIP dealing with weird imports

--- test/flatbuffers/js/generate-flatbuffer.js
+++ test/flatbuffers/js/generate-flatbuffer.js
@@ -2,65 +2,81 @@ import test from 'ava';
 import { flatbuffers } from 'flatbuffers';
 
 const { MSG_TYPES } = require('../../../src/js/index');
-const TopicData = require('../../../dist/flatbuffers/js/topicData_generated').ubii.topicData.TopicData;
+
+/*const TopicData = require('../../../dist/flatbuffers/js/topicData_generated').ubii.topicData.TopicData;
 const TopicDataContent = require('../../../dist/flatbuffers/js/topicData_generated').ubii.topicData.TopicDataContent;
 const TopicDataRecord = require('../../../dist/flatbuffers/js/topicDataRecord_generated').ubii.topicData.TopicDataRecord;
 const DataStructure = require('../../../dist/flatbuffers/js/topicDataRecord_generated').ubii.dataStructures.DataStructure;
 const Timestamp = require('../../../dist/flatbuffers/js/timestamp_generated').ubii.dataStructures.Timestamp;
-const Vector3 = require('../../../dist/flatbuffers/js/vector3_generated').ubii.dataStructures.Vector3;
+const Vector3 = require('../../../dist/flatbuffers/js/vector3_generated').ubii.dataStructures.Vector3;*/
+
+//let ubii = {};
+/*let ubiiTopicData = require('../../../dist/flatbuffers/js/topicData_generated').ubii;
+ubii = { ...ubii, ...ubiiTopicData };
+console.info(ubii);
+let ubiiTopicDataRecord = require('../../../dist/flatbuffers/js/topicDataRecord_generated').ubii;
+ubii = { ...ubii, ...ubiiTopicDataRecord };
+console.info('\n\n');
+console.info(ubii);*/
+let ubiiTimestamp = require('../../../dist/flatbuffers/js/timestamp_generated').ubii;
+console.info(ubiiTimestamp);
+let ubiiVector3 = require('../../../dist/flatbuffers/js/vector3_generated').ubii;
+console.info(ubiiVector3);
+
+const ubii = Object.assign({}, ubiiTimestamp, ubiiVector3);
+console.info(ubii);
+
+const TopicData = ubii.topicData.TopicData;
+const TopicDataContent = ubii.topicData.TopicDataContent;
+const TopicDataRecord = ubii.topicData.TopicDataRecord;
+const DataStructure = ubii.dataStructures.DataStructure;
+const Timestamp = ubii.dataStructures.Timestamp;
+const Vector3 = ubii.dataStructures.Vector3;
+
+/* helper methods */
+
+let createFlatbufferTopicDataVector3 = (topicString, timestamp, vector3) => {
+    let builder = new flatbuffers.Builder(0);
+
+    let topic = builder.createString(topicString);
+
+    DataStructure.startDataStructure(builder);
+    DataStructure.addVector3(builder, Vector3.createVector3(builder, vector3.x, vector3.y, vector3.z));
+    let dataStructure = DataStructure.endDataStructure(builder);
+
+    TopicDataRecord.startTopicDataRecord(builder);
+    TopicDataRecord.addTopic(builder, topic);
+    TopicDataRecord.addTimestamp(builder, Timestamp.createTimestamp(builder, timestamp.seconds, timestamp.nanos));
+    TopicDataRecord.addData(builder, dataStructure);
+    let topicDataRecord = TopicDataRecord.endTopicDataRecord(builder);
+
+    TopicData.startTopicData(builder);
+    TopicData.addContentType(builder, TopicDataContent.topic_data_record);
+    TopicData.addContent(builder, topicDataRecord);
+    let topicData = TopicData.endTopicData(builder);
+
+    builder.finish(topicData);
+    let topicDataBuffer = builder.dataBuffer();
+    return topicDataBuffer;
+};
 
 /* prepare tests */
 
-/*test.beforeEach(t => {
-    t.context.msgType = MSG_TYPES.ERROR;
-    t.context.msgTypeUndefined = 'ubii.service.reply.SomethingUndefined';
-});*/
-
 /* run tests */
 
-test('create a TopicData flatbuffer', t => {
-    //console.info(flatbuffers);
+test('create a TopicData flatbuffer, then read it back in', t => {
     let topicString = '/my/test/topic';
+    let timestamp = { seconds: 1, nanos: 2 };
+    let vector3 = { x: 1.1, y: 2.2, z: 3.3 };
 
-    //// VERSION 1
-    //// createXYZ calls
-    /*let builder = new flatbuffers.Builder(0);
+    // create buffer
+    let bufferTopicData = createFlatbufferTopicDataVector3(topicString, timestamp, vector3);
 
-    //let vector3 = Vector3.createVector3(builder, 1.1, 2.2, 3.3);
-    DataStructure.startDataStructure(builder);
-    DataStructure.addVector3(builder, Vector3.createVector3(builder, 1.1, 2.2, 3.3));
-    let data = DataStructure.endDataStructure(builder);
+    // read buffer
+    let topicData = TopicData.getRootAsTopicData(bufferTopicData);
 
-    //let topic = builder.createString(topicString);
-    //let timestamp = Timestamp.createTimestamp(builder, 1, 5000);
+    let topicDataRecord = new TopicDataRecord();
+    topicData.content(topicDataRecord);
 
-    let topicDataRecord = TopicDataRecord.createTopicDataRecord(
-        builder,
-        builder.createString(topicString),
-        Timestamp.createTimestamp(builder, 1, 5000),
-        data
-    );
-    let topicData = TopicData.createTopicData(
-        builder,
-        TopicDataContent.topic_data_record,
-        topicDataRecord
-    );
-    builder.finish(topicData);*/
-
-    //// VERSION 2
-    //// startXYZ / endXYZ calls
-    let builder = new flatbuffers.Builder(0);
-
-    TopicData.startTopicData(builder);
-
-    TopicDataRecord.startTopicDataRecord(builder);
-    TopicDataRecord.addTopic(builder, builder.createString(topicString));
-    let topicDataRecord = TopicDataRecord.endTopicDataRecord(builder);
-
-    TopicData.addContentType(builder, TopicDataContent.topic_data_record);
-    TopicData.addContent(builder, topicDataRecord);
-
-    let topicData = TopicData.endTopicData(builder);
-
-    console.info(topicData);
+    console.info(topicDataRecord.data().vector3());
 });
