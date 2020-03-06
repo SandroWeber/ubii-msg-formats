ae246f6c34e3f26b60c09987fd62a1d98ee4d5db
Sandro Weber <webers@in.tum.de>
2020-03-06 11:00:06 +0100

    working first test

--- test/flatbuffers/js/generate-flatbuffer.js
+++ test/flatbuffers/js/generate-flatbuffer.js
@@ -3,38 +3,16 @@ import { flatbuffers } from 'flatbuffers';
 
 const { MSG_TYPES } = require('../../../src/js/index');
 
-/*const TopicData = require('../../../dist/flatbuffers/js/topicData_generated').ubii.topicData.TopicData;
+const TopicData = require('../../../dist/flatbuffers/js/topicData_generated').ubii.topicData.TopicData;
 const TopicDataContent = require('../../../dist/flatbuffers/js/topicData_generated').ubii.topicData.TopicDataContent;
 const TopicDataRecord = require('../../../dist/flatbuffers/js/topicDataRecord_generated').ubii.topicData.TopicDataRecord;
 const DataStructure = require('../../../dist/flatbuffers/js/topicDataRecord_generated').ubii.dataStructures.DataStructure;
 const Timestamp = require('../../../dist/flatbuffers/js/timestamp_generated').ubii.dataStructures.Timestamp;
-const Vector3 = require('../../../dist/flatbuffers/js/vector3_generated').ubii.dataStructures.Vector3;*/
+const Vector3 = require('../../../dist/flatbuffers/js/vector3_generated').ubii.dataStructures.Vector3;
 
-//let ubii = {};
-/*let ubiiTopicData = require('../../../dist/flatbuffers/js/topicData_generated').ubii;
-ubii = { ...ubii, ...ubiiTopicData };
-console.info(ubii);
-let ubiiTopicDataRecord = require('../../../dist/flatbuffers/js/topicDataRecord_generated').ubii;
-ubii = { ...ubii, ...ubiiTopicDataRecord };
-console.info('\n\n');
-console.info(ubii);*/
-let ubiiTimestamp = require('../../../dist/flatbuffers/js/timestamp_generated').ubii;
-console.info(ubiiTimestamp);
-let ubiiVector3 = require('../../../dist/flatbuffers/js/vector3_generated').ubii;
-console.info(ubiiVector3);
+/* helpers */
 
-const ubii = Object.assign({}, ubiiTimestamp, ubiiVector3);
-console.info(ubii);
-
-const TopicData = ubii.topicData.TopicData;
-const TopicDataContent = ubii.topicData.TopicDataContent;
-const TopicDataRecord = ubii.topicData.TopicDataRecord;
-const DataStructure = ubii.dataStructures.DataStructure;
-const Timestamp = ubii.dataStructures.Timestamp;
-const Vector3 = ubii.dataStructures.Vector3;
-
-/* helper methods */
-
+/*
 let createFlatbufferTopicDataVector3 = (topicString, timestamp, vector3) => {
     let builder = new flatbuffers.Builder(0);
 
@@ -42,6 +20,51 @@ let createFlatbufferTopicDataVector3 = (topicString, timestamp, vector3) => {
 
     DataStructure.startDataStructure(builder);
     DataStructure.addVector3(builder, Vector3.createVector3(builder, vector3.x, vector3.y, vector3.z));
+    let dataStructure = DataStructure.endDataStructure(builder);
+
+    TopicDataRecord.startTopicDataRecord(builder);
+    TopicDataRecord.addTopic(builder, topic);
+    TopicDataRecord.addTimestamp(builder, Timestamp.createTimestamp(builder, timestamp.seconds, timestamp.nanos));
+    TopicDataRecord.addData(builder, dataStructure);
+    let topicDataRecord = TopicDataRecord.endTopicDataRecord(builder);
+
+    TopicData.startTopicData(builder);
+    TopicData.addContentType(builder, TopicDataContent.topic_data_record);
+    TopicData.addContent(builder, topicDataRecord);
+    let topicData = TopicData.endTopicData(builder);
+
+    builder.finish(topicData);
+    let topicDataBuffer = builder.dataBuffer();
+    return topicDataBuffer;
+};
+*/
+
+let testBool = true;
+let testStringList = ['one', 'two', 'three'];
+let testVector3 = { x: 1.1, y: 2.2, z: 3.3 };
+
+/**
+ * Creates a TopicData object with the following data inside:
+ * bool: true
+ * stringList: ['one', 'two', 'three']
+ * vector3: (1.1, 2.2, 3.3)
+ * 
+ * @param {*} topicString 
+ * @param {*} timestamp 
+ */
+let createFlatbufferTopicData = (topicString, timestamp) => {
+    let builder = new flatbuffers.Builder(0);
+
+    let topic = builder.createString(topicString);
+    let stringOne = builder.createString(testStringList[0]);
+    let stringTwo = builder.createString(testStringList[1]);
+    let stringThree = builder.createString(testStringList[2]);
+    let stringList = DataStructure.createStringListVector(builder, [stringOne, stringTwo, stringThree]);
+
+    DataStructure.startDataStructure(builder);
+    DataStructure.addVector3(builder, Vector3.createVector3(builder, testVector3.x, testVector3.y, testVector3.z));
+    DataStructure.addBool(builder, testBool);
+    DataStructure.addStringList(builder, stringList);
     let dataStructure = DataStructure.endDataStructure(builder);
 
     TopicDataRecord.startTopicDataRecord(builder);
@@ -67,16 +90,28 @@ let createFlatbufferTopicDataVector3 = (topicString, timestamp, vector3) => {
 test('create a TopicData flatbuffer, then read it back in', t => {
     let topicString = '/my/test/topic';
     let timestamp = { seconds: 1, nanos: 2 };
-    let vector3 = { x: 1.1, y: 2.2, z: 3.3 };
 
     // create buffer
-    let bufferTopicData = createFlatbufferTopicDataVector3(topicString, timestamp, vector3);
+    //let bufferTopicData = createFlatbufferTopicDataVector3(topicString, timestamp, vector3);
+    let bufferTopicData = createFlatbufferTopicData(topicString, timestamp);
 
     // read buffer
     let topicData = TopicData.getRootAsTopicData(bufferTopicData);
 
-    let topicDataRecord = new TopicDataRecord();
-    topicData.content(topicDataRecord);
+    /*let topicDataRecord = new TopicDataRecord();
+    topicData.content(topicDataRecord);*/
+    let topicDataRecord = topicData.content(new TopicDataRecord());
 
-    console.info(topicDataRecord.data().vector3());
+    let vector3 = topicDataRecord.data().vector3(new Vector3());
+
+    t.is(vector3.x().toFixed(1), testVector3.x.toFixed(1));
+    t.is(vector3.y().toFixed(1), testVector3.y.toFixed(1));
+    t.is(vector3.z().toFixed(1), testVector3.z.toFixed(1));
+
+    t.is(topicDataRecord.data().bool(), testBool);
+
+    t.is(topicDataRecord.data().stringListLength(), testStringList.length);
+    t.is(topicDataRecord.data().stringList(0), testStringList[0]);
+    t.is(topicDataRecord.data().stringList(1), testStringList[1]);
+    t.is(topicDataRecord.data().stringList(2), testStringList[2]);
 });
