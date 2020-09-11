c440ef3c6451d8fa339fa50e14671e6954467f5f
Sandro Weber <webers@in.tum.de>
2020-09-11 16:49:33 +0200

    interactions removed

---
+++ scripts/full-compile.js
@@ -0,0 +1,79 @@
+const childProcess = require('child_process');
+const path = require('path');
+
+let compileProtoPython = () => {
+  return new Promise((resolve, reject) => {
+    // compile .proto via python
+    let pathProtoCompile = path.join(__dirname, 'proto-compile.py');
+    console.info(pathProtoCompile);
+    let processPythonProtoCompile = childProcess.spawn('python3', [pathProtoCompile]);
+
+    // Takes stdout data from script which executed
+    // with arguments and send this data to res object
+    processPythonProtoCompile.stdout.on('data', function (data) {
+      console.info(data.toString());
+    });
+
+    processPythonProtoCompile.on('close', (code) => {
+      console.log(`python .proto compile process close all stdio with code ${code}`);
+      if (code === 0) {
+        return resolve(code);
+      } else {
+        return reject(code);
+      }
+    });
+  });
+};
+
+let compileProtobufJS = () => {
+  return new Promise((resolve, reject) => {
+    let pathProtobufJS = path.join(__dirname, 'generate-protobuf-static.js');
+    var processProtobufJS = childProcess.fork(pathProtobufJS);
+
+    processProtobufJS.on('error', function (err) {
+      console.error(err);
+      return reject();
+    });
+
+    processProtobufJS.on('exit', function (code) {
+      var err = code === 0 ? null : new Error('exit code ' + code);
+      console.error(err);
+
+      if (code === 0) {
+        return resolve();
+      }
+    });
+  });
+};
+
+let compileConstants = () => {
+  return new Promise((resolve, reject) => {
+    let pathConstants = path.join(__dirname, 'compile-constants.js');
+    var processConstants = childProcess.fork(pathConstants);
+
+    processConstants.on('error', function (err) {
+      console.error(err);
+      return reject();
+    });
+
+    processConstants.on('exit', function (code) {
+      var err = code === 0 ? null : new Error('exit code ' + code);
+      console.error(err);
+
+      if (code === 0) {
+        return resolve();
+      }
+    });
+  });
+};
+
+(async function () {
+  // compile .proto files via python
+  await compileProtoPython();
+
+  // compile protobuf.js
+  await compileProtobufJS();
+
+  // compile constants
+  await compileConstants();
+})();
--- scripts/generate-protobuf-static.js
+++ scripts/generate-protobuf-static.js
@@ -3,49 +3,44 @@ const path = require('path');
 
 const pbjs = require('protobufjs/cli/pbjs');
 
-
 (function () {
-    let getFilesSync = (dir, filter) => {
-        if (!fs.existsSync(dir)) {
-            console.log('no directory ', dir);
-            return;
-        }
+  let getFilesSync = (dir, filter) => {
+    if (!fs.existsSync(dir)) {
+      console.log('no directory ', dir);
+      return;
+    }
 
-        let files = [];
-        let list = fs.readdirSync(dir);
-        for (let i = 0; i < list.length; i++) {
-            let newPath = path.join(dir, list[i]);
-            let stat = fs.lstatSync(newPath);
-            if (stat.isDirectory()) {
-                getFilesSync(newPath, filter).forEach((file) => {
-                    files.push(file);
-                });
-            }
-            else if (newPath.indexOf(filter) >= 0) {
-                files.push(newPath);
-            }
-        }
+    let files = [];
+    let list = fs.readdirSync(dir);
+    for (let i = 0; i < list.length; i++) {
+      let newPath = path.join(dir, list[i]);
+      let stat = fs.lstatSync(newPath);
+      if (stat.isDirectory()) {
+        getFilesSync(newPath, filter).forEach((file) => {
+          files.push(file);
+        });
+      } else if (newPath.indexOf(filter) >= 0) {
+        files.push(newPath);
+      }
+    }
 
-        return files;
-    };
+    return files;
+  };
 
-    let generateCode = (protobufDirectory) => {
-        let outputFile = path.resolve(__dirname + '/../dist/js/protobuf.js');
-        let params = [
-            '--target', 'static-module',
-            '--wrap', 'commonjs',
-            '--out', outputFile
-        ];
-        let files = getFilesSync(protobufDirectory, '.proto');
-        params.push(...files);
+  let generateCode = (protobufDirectory) => {
+    let outputFile = path.resolve(__dirname + '/../dist/js/protobuf.js');
+    let params = ['--target', 'static-module', '--wrap', 'commonjs', '--out', outputFile];
+    let files = getFilesSync(protobufDirectory, '.proto');
+    params.push(...files);
 
-        pbjs.main(params,
-            function (err, output) {
-                if (err) throw err;
-                // do something with output
-            });
-    };
+    pbjs.main(params, function (err, output) {
+      if (err) throw err;
+      // do something with output
+    });
 
-    let protobufDirectory = path.normalize(__dirname + '/../src/proto');
-    generateCode(protobufDirectory);
+    console.info('protobuf.js compiled');
+  };
+
+  let protobufDirectory = path.normalize(__dirname + '/../src/proto');
+  generateCode(protobufDirectory);
 })();
