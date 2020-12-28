0a7ae710547c3ad46f92dc6450d14860e9174165
Sandro Weber <webers@in.tum.de>
2020-12-28 22:17:37 +0100

    ava major update, other dependencies

--- ava.config.js
+++ ava.config.js
@@ -1,16 +1,12 @@
 export default {
-	"files": [
-		"test/protobuf/js/protobufUtils.js",
-		"test/protobuf/js/protobufTranslator.js"
-	],
-	"sources": [
-		"**/*.{js,jsx}",
-		"!dist/**/*"
-	],
-	"cache": false,
-	"failFast": false,
-	"failWithoutAssertions": false,
-	"tap": false,
-	"verbose": false,
-	"compileEnhancements": false
+  babel: {
+    compileEnhancements: true
+  },
+  files: ['test/protobuf/js/protobufUtils.js', 'test/protobuf/js/protobufTranslator.js'],
+  ignoredByWatcher: ['**/*.{js,jsx}', '!dist/**/*'],
+  cache: false,
+  failFast: false,
+  failWithoutAssertions: false,
+  tap: false,
+  verbose: false
 };
