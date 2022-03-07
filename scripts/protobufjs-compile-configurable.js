9b447b068e78ce1c6e035fc3e74ade25b671e088
Sandro Weber <webers@in.tum.de>
2022-03-07 09:42:00 +0100

    WIP

---
+++ scripts/protobufjs-compile-configurable.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+
+const pbjs = require('protobufjs/cli/pbjs');
+
+const inputDirectoryRelativePath = '/proto';
+const outputRelativePath = '/proto/nrp-engine_msgs-protobufjs.js';
+
+(function () {
+  let getFilesSync = (dir, filter) => {
+    if (!fs.existsSync(dir)) {
+      console.log('no directory ', dir);
+      return;
+    }
+
+    let files = [];
+    let list = fs.readdirSync(dir);
+    for (let i = 0; i < list.length; i++) {
+      let newPath = path.join(dir, list[i]);
+      let stat = fs.lstatSync(newPath);
+      if (stat.isDirectory()) {
+        getFilesSync(newPath, filter).forEach((file) => {
+          files.push(file);
+        });
+      } else if (newPath.indexOf(filter) >= 0) {
+        files.push(newPath);
+      }
+    }
+
+    return files;
+  };
+
+  let generateCode = (protobufDirectory) => {
+    let outputFile = path.resolve(__dirname + outputRelativePath);
+    let params = ['--target', 'static-module', '--wrap', 'commonjs', '--out', outputFile];
+    let files = getFilesSync(protobufDirectory, '.proto');
+    params.push(...files);
+
+    pbjs.main(params, function (err, output) {
+      if (err) throw err;
+      // do something with output
+    });
+
+    console.info('protobuf.js compiled');
+  };
+
+  let protobufDirectory = path.normalize(__dirname + inputDirectoryRelativePath);
+  generateCode(protobufDirectory);
+})();
